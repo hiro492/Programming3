78af749ecee8c8ec67590e2c4beeacb38519e3b7
hiro492 <hirocat2019@gmail.com>
2024-06-14 15:54:45 +0900

    first commit

---
+++ My password app/main.js
@@ -0,0 +1,39 @@
+'use strict';
+{
+  const slider = document.getElementById('slider');
+  const btn = document.getElementById('btn');
+
+  slider.addEventListener('input', () => {
+    const passwordLength = document.getElementById('password-length');
+
+    passwordLength.textContent = slider.value;
+  });
+
+  btn.addEventListener('click', () => {
+    const result = document.getElementById('result');
+    const numbersCheckbox = document.getElementById('numbers-checkbox');
+    const symbolsCheckbox = document.getElementById('symbols-cherckbox');
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!"#$%&()*+,-./:;<=>?@[^_{|}~';
+    let password = '';
+    let seed = letters + letters.toLocaleUpperCase();
+
+    if (numbersCheckbox.checked && symbolsCheckbox.checked) {
+      seed += numbers + symbols;
+    } else if (numbersCheckbox.checked) {
+      seed += numbers;
+    } else if (symbolsCheckbox.checked) {
+      seed += symbols;
+    } else {
+    }
+
+    for (let i = 0; i < slider.value; i++) {
+      password += seed[Math.floor(Math.random() * seed.length)]
+    }
+    
+    result.textContent = password;
+
+
+  });
+}
