d889a653992dc56ecf15cc86a63b06c23ab81ceb
hiro492 <hirocat2019@gmail.com>
2024-06-25 14:15:55 +0900

    scroll ver1

--- ScrollTrigger.js
+++ ScrollTrigger.js
@@ -1,9 +1,9 @@
 //スクロールをするたびにアニメーションを行う設定  
 $('.fadeDounTrigger').on('inview', function(event, isInView) {
     if (isInView) {//表示領域に入った時
-      $(this).addClass('animate__animated animate__fadeIn');//クラス名が付与
+      $(this).addClass('animate__fadeIn');//クラス名が付与
     } else {//表示領域から出た時
-      $(this).removeClass('animate__animated animate__fadeIn');//クラス名が除去
+      $(this).removeClass('animate__fadeIn');//クラス名が除去
     }
   });
 
